9cadac2aa003910611bea8227a9a3d11f71d9cb8
irvin espinal <irvin@floorsoft.com>
2020-05-13 18:40:18 -0400

    Updates

---
+++ precache-manifest.d5e064d66ca9d0f0341a6e3a6596ddc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28359c16048bd051e0024ee09730dea",
+    "url": "/app-loteria/index.html"
+  },
+  {
+    "revision": "3e4f132a1c4d542d2879",
+    "url": "/app-loteria/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48a94066fe7b59975684",
+    "url": "/app-loteria/static/js/2.f75f716f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-loteria/static/js/2.f75f716f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e4f132a1c4d542d2879",
+    "url": "/app-loteria/static/js/main.4571c09e.chunk.js"
+  },
+  {
+    "revision": "17d8d660bec3a0b47909",
+    "url": "/app-loteria/static/js/runtime-main.33a207d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app-loteria/static/media/logo.5d5d9eef.svg"
+  }
+]);
